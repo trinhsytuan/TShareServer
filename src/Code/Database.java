8e9e1d2fd32bbf68d8cf03516caf95b5dde9c1df
Trinh Sy Tuan <trinhsytuan@gmail.com>
2023-03-10 09:23:03 +0700

    Add Server Socket and fix bug

--- src/Code/Database.java
+++ src/Code/Database.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package tshareserver;
+package Code;
 
 import java.net.InetAddress;
 import java.sql.Connection;
--- src/Code/Message.java
+++ src/Code/Message.java
@@ -1,4 +1,4 @@
-package tshareserver;
+package Code;
 
 import java.io.Serializable;
 import java.util.Vector;
